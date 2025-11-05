9ae5558444ea5e55c22f576652cfdd66efc3719d
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-11-05 14:35:25 +0100

    Fix transformations

--- dataloaders/dataloader.py
+++ dataloaders/dataloader.py
@@ -584,14 +584,25 @@ class iDIL_IMAGENET_R(iDIL_Dataset):
                 self.archive_targets.append(target)
     
     def load_dataset(self, t, train=True):
-        self.transform = transforms.Compose([
-            transforms.Resize(256),
-            transforms.RandomCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=(0.485, 0.456, 0.406),
-                                std=(0.229, 0.224, 0.225))
-        ])
+        train_transform = transforms.Compose([
+                transforms.Resize(256),
+                transforms.RandomCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406),
+                                    (0.229, 0.224, 0.225)),
+            ])
+
+        test_transform = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406),
+                                    (0.229, 0.224, 0.225)),
+            ])
+        
+        self.transform = train_transform if train else test_transform
+
 
         if train:
             self.data = self.archive_data[t] 
