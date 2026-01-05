a040f2414891e77ef9740610e44ba683f4a74dd0
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2026-01-05 17:43:30 +0100

    Reorganize directories

---
+++ experiments/intact/coda_prompt/dil_imagenet-r_15_tasks.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --job-name=CODA-P_dil_imagenet-r_15_tasks
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --partition=dgx
+
+
+
+source activate prompt_intact
+
+# bash experiments/imagenet-r.sh
+# experiment settings
+DATASET=DIL_ImageNet_R
+N_CLASS=200
+
+# save directory
+# PLEASE CHANGE THIS!!!
+OUTDIR=./${DATASET}/15-task
+
+# hard coded inputs
+GPUID='0'
+CONFIG=configs/dil_imnet-r_prompt_15_tasks.yaml
+REPEAT=1
+OVERWRITE=0
+
+###############################################################
+
+# process inputs
+mkdir -p $OUTDIR
+
+# CODA-P
+#
+# prompt parameter args:
+#    arg 1 = prompt component pool size
+#    arg 2 = prompt length
+#    arg 3 = ortho penalty loss weight - with updated code, now can be 0!
+VAR_LOSS_SCALES=("0.1")
+INTERNAL_REPRESENTATION_DRIFT_REG_SCALES=("0.0001" "0.001" "0.01" "0.1")
+FEATURE_LOSS_SCALES=("0.1")
+
+for var in "${VAR_LOSS_SCALES[@]}"; do
+  for out in "${INTERNAL_REPRESENTATION_DRIFT_REG_SCALES[@]}"; do
+    for drift in "${FEATURE_LOSS_SCALES[@]}"; do
+        LOGDIR=${OUTDIR}/coda-p/var${var}_out${out}_drift${drift}
+        mkdir -p $LOGDIR
+        python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+          --learner_type prompt --learner_name CODAPrompt \
+          --prompt_param 100 8 0.0 \
+          --use_interval_activation \
+          --log_dir $LOGDIR \
+          --var_loss_scale $var \
+          --internal_repr_drift_loss_scale $out \
+          --feature_loss_scale $drift \
+          --use_align_loss
+    done
+  done
+done
---
+++ experiments/intact/l2p/dil_imagenet-r_15_tasks.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --job-name=L2P_dil_imagenet-r_15_tasks
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --partition=dgx
+
+
+source activate prompt_intact
+
+# bash experiments/imagenet-r.sh
+# experiment settings
+DATASET=DIL_ImageNet_R
+N_CLASS=200
+
+# save directory
+# PLEASE CHANGE THIS!!!
+OUTDIR=./${DATASET}/15-task
+
+# hard coded inputs
+GPUID='0'
+CONFIG=configs/dil_imnet-r_prompt_15_tasks.yaml
+REPEAT=1
+OVERWRITE=0
+
+###############################################################
+
+# process inputs
+mkdir -p $OUTDIR
+
+# L2P++
+#
+# prompt parameter args:
+#    arg 1 = e-prompt pool size (# tasks)
+#    arg 2 = e-prompt pool length
+#    arg 3 = -1 -> shallow, 1 -> deep
+VAR_LOSS_SCALES=("0.01")
+INTERNAL_REPRESENTATION_DRIFT_REG_SCALES=("0.0001" "0.001" "0.01" "0.1")
+FEATURE_LOSS_SCALES=("0.1")
+
+for var in "${VAR_LOSS_SCALES[@]}"; do
+  for out in "${INTERNAL_REPRESENTATION_DRIFT_REG_SCALES[@]}"; do
+    for drift in "${FEATURE_LOSS_SCALES[@]}"; do
+        LOGDIR=${OUTDIR}/l2p/var${var}_out${out}_drift${drift}
+        mkdir -p $LOGDIR
+        python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+            --learner_type prompt --learner_name L2P \
+            --prompt_param 30 20 -1 \
+            --use_interval_activation \
+            --log_dir $LOGDIR \
+            --var_loss_scale $var \
+            --internal_repr_drift_loss_scale $out \
+            --feature_loss_scale $drift \
+            --use_align_loss
+    done
+  done
+done
