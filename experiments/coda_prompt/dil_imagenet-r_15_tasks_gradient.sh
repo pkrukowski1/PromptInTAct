dd9068b9bcf70e19e726a98162502174511bc6f2
OneAndZero24 <oneandzero24@gmail.com>
2026-07-26 20:28:39 +0200

    .

---
+++ experiments/coda_prompt/dil_imagenet-r_15_tasks_gradient.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --job-name=CODA-P_dil_imagenet-r_15_grad
+#SBATCH --qos=big
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --partition=dgx
+
+source activate prompt_intact
+
+# experiment settings
+DATASET=DIL_ImageNet_R
+N_CLASS=200
+
+# save directory
+OUTDIR=./${DATASET}/15-task_gradient_cosine
+
+# hard coded inputs
+GPUID='0'
+CONFIG=configs/dil_imnet-r_prompt_15_tasks.yaml
+REPEAT=1
+OVERWRITE=1
+
+###############################################################
+
+mkdir -p $OUTDIR
+
+CODA_P_POOL_SIZES=(196)
+CODA_P_LENGTHS=(8)
+VAR_LOSS_SCALE=0.1
+INTERNAL_REP_DRIFT_SCALE=0.001
+FEATURE_LOSS_SCALE=0.1
+
+LOGDIR=${OUTDIR}/coda-p/pool${pool}_len${length}_var${VAR_LOSS_SCALE}_out${INTERNAL_REP_DRIFT_SCALE}_drift${FEATURE_LOSS_SCALE}
+mkdir -p $LOGDIR
+
+python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+  --learner_type prompt --learner_name CODAPrompt \
+  --prompt_param 100 8 0.0 \
+  --use_interval_activation \
+  --log_dir $LOGDIR \
+  --var_loss_scale $VAR_LOSS_SCALE \
+  --internal_repr_drift_loss_scale $INTERNAL_REP_DRIFT_SCALE \
+  --feature_loss_scale $FEATURE_LOSS_SCALE \
+  --use_align_loss \
+  --gradient_analysis
